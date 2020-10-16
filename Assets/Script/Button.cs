d792b7963015f40c82a7dbe2cd8804614fee868a
JGuo99 <jguo0310@gmail.com>
2020-10-16 18:32:45 -0400

    Application Completion

---
+++ Assets/Script/Button.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Button : MonoBehaviour
+{
+    public GameObject button;
+    public Animator anima;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GameObject.Find("VButton");
+        button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        button.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+        
+        // anima = GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button pressed");
+        anima.SetTrigger("workoutChange");
+        audioSource.Play();        
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button released");
+    }
+
+    void Update(){}
+}
---
+++ Assets/Script/Destory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destory : MonoBehaviour
+{
+   	public float timeLeft = 15.0f; // seconds
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0){
+            timeLeft -= Time.deltaTime;
+            if (timeLeft <= 0) {
+                Destroy(this.gameObject);
+      	}
+      }
+    }
+}
